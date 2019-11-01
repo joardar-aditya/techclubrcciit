0c3063b8d66a0b40f99681e33b96762d25c0a423
joardar-aditya <youngjoardar@gmail.com>
2019-11-01 23:48:36 +0530

    sign in basic added

--- app/src/main/java/com/hummbletech/techclubrcciit/SigninActivity.java
+++ app/src/main/java/com/hummbletech/techclubrcciit/SigninActivity.java
@@ -1,10 +1,32 @@
 package com.hummbletech.techclubrcciit;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 public class SigninActivity extends AppCompatActivity {
+
+
+    GoogleSignInClient signInClient;
+    private static int RC_SIGN_IN = 007;
+
+    Intent start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +34,65 @@ public class SigninActivity extends AppCompatActivity {
         setContentView(R.layout.activity_signin);
 
 
+        start = new Intent(SigninActivity.this, MainActivity.class);
+        TextView welcome_text = findViewById(R.id.textView2);
+        SignInButton button = findViewById(R.id.sign_in_button);
+
+        AnimatorSet set = (AnimatorSet) AnimatorInflater.loadAnimator(getBaseContext(), R.animator.anime1);
+        set.setTarget(welcome_text);
+        set.start();
+
+        GoogleSignInOptions options = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+
+        signInClient = GoogleSignIn.getClient(this, options);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signInIntent = signInClient.getSignInIntent();
+                startActivityForResult(signInIntent, RC_SIGN_IN);
+            }
+        });
+
+
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == RC_SIGN_IN){
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        if(account != null){
+            startActivity(start);
+        }
+    }
+
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            Toast.makeText(getBaseContext(), "Welcome " + account.getDisplayName(),Toast.LENGTH_SHORT).show();
+            startActivity(start);
+
+            // Signed in successfully, show authenticated UI.
+
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w("ERROR", "signInResult:failed code=" + e.getStatusCode());
+        }
     }
 }
