3431520498fd3c66e934380ee335865327a277fe
joardar-aditya <youngjoardar@gmail.com>
2019-11-01 19:05:32 +0530

    sign in activity added

---
+++ app/src/main/java/com/hummbletech/techclubrcciit/SigninActivity.java
@@ -0,0 +1,16 @@
+package com.hummbletech.techclubrcciit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SigninActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+
+        
+    }
+}
